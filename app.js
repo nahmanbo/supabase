286f098a58077ed8d35a6739abb8811cc4eacb4f
nahmanbo <nhmanbo2003@gmail.com>
2025-07-15 12:59:58 +0300

    refactor server

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 import express from "express";
-import userRouter from "./routes/userRouter.js";
+import dotenv from "dotenv";
+import authRouter from "./routes/authRouter.js";
 import productRouter from "./routes/productRouter.js";
 import { logger } from "./utils/loggerHelper.js";
 
+dotenv.config();
 const PORT = process.env.PORT || 1234;
 const server = express();
 
 //-------------Middleware---------------
-server.use(express.json());  
-server.use(logger);         
+server.use(express.json());
+server.use(logger);
 
 //-------------Routes---------------
-server.use("/user", userRouter);
-server.use("/product", productRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/products", productRouter);
 
 //-------------Start server---------------
 server.listen(PORT, () => {
